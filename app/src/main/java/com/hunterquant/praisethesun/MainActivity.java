dcc663db0460117daa2981ba1b23bacdf0fffdcb
Hunter Quant <hunterdquant@gmail.com>
2016-06-11 21:43:40 -0400

    Added the ability to share the weather

--- app/src/main/java/com/hunterquant/praisethesun/MainActivity.java
+++ app/src/main/java/com/hunterquant/praisethesun/MainActivity.java
@@ -1,11 +1,16 @@
 package com.hunterquant.praisethesun;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/hunterquant/praisethesun/WeatherDetailsActivity.java
+++ app/src/main/java/com/hunterquant/praisethesun/WeatherDetailsActivity.java
@@ -1,25 +1,27 @@
 package com.hunterquant.praisethesun;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.zip.Inflater;
 
 public class WeatherDetailsActivity extends ActionBarActivity {
 
     public static final String DETAILS = "DETAILS";
-
-    static String weatherDetails = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,16 +29,9 @@ public class WeatherDetailsActivity extends ActionBarActivity {
         setContentView(R.layout.activity_weather_details);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new WeatherDetailsFragment())
                     .commit();
         }
-        weatherDetails = getIntent().getStringExtra(DETAILS);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.weather_details, menu);
-        return true;
     }
 
     @Override
@@ -49,14 +44,38 @@ public class WeatherDetailsActivity extends ActionBarActivity {
             intent.setClass(this, SettingsActivity.class);
             startActivity(intent);
             return true;
+        } else if (id == R.id.action_map) {
+            openPreferredLocationMap();
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    public static class PlaceholderFragment extends Fragment {
+    private void openPreferredLocationMap() {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String loc = prefs.getString(getString(R.string.pref_location_key), getString(R.string.pref_default_location));
 
-        public PlaceholderFragment() {
+        Uri geoLoc = Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", loc).build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+        intent.setData(geoLoc);
+
+        if (intent.resolveActivity(this.getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("TEST", "Couldn't get " + loc + ", no map ");
+        }
+    }
+
+    public static class WeatherDetailsFragment extends Fragment {
+
+        public final String HASHTAG = " #PraiseTheSun!";
+
+        static String weatherDetails = null;
+
+        public WeatherDetailsFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -64,8 +83,31 @@ public class WeatherDetailsActivity extends ActionBarActivity {
                                  Bundle savedInstanceState) {
 
             View rootView = inflater.inflate(R.layout.fragment_weather_details, container, false);
-            ((TextView)rootView.findViewById(R.id.hello)).setText(weatherDetails);
+            Intent intent = getActivity().getIntent();
+            if (intent != null && intent.hasExtra(DETAILS)) {
+                weatherDetails = intent.getStringExtra(DETAILS);
+                ((TextView) rootView.findViewById(R.id.weatherDetails)).setText(weatherDetails);
+            }
             return rootView;
+        }
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detail_menu, menu);
+            MenuItem item = menu.findItem(R.id.menu_item_share);
+            ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            if (shareActionProvider != null) {
+                shareActionProvider.setShareIntent(createShareIntent());
+            }
+        }
+
+        private Intent createShareIntent() {
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT,  weatherDetails + HASHTAG);
+            return intent;
+
         }
     }
 }
